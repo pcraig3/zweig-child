134a406697b25613eeb721fd819f820c7e27e0d2
Paul Craig <pcraig3@uwo.ca>
2014-11-25 21:00:12 -0500

    Scrolling actions controlled by a new plugin.

--- js/scroll.js
+++ js/scroll.js
@@ -2,33 +2,14 @@ jQuery(function( $ ){
 
 	$(document).ready(function() {
 			
-			// The default axis is 'y', but in this demo, I want to scroll both
-			// You can modify any default like this
-			//$.localScroll.defaults.axis = 'xy';
-			
-			/**
-			 * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
-			 * also affect the >> and << links. I want every link in the page to scroll.
-			 */
-			
-			var $offset_top = 0;
+		/* using mahlihu's scroll-to-id plugin	
 
-			if( $( 'body' ).hasClass('admin-bar') ) 
-				var $offset_top = $('#wpadminbar').height();
-
-			$.localScroll({
-				target: 'body', // could be a selector or a jQuery object too.
-				queue:true,
-				duration:300,
-				hash:true,
-				//if we're administrators, this breaks.
-				offset: {left: 0, top: - $offset_top },
-				onBefore:function( e, anchor, $target ){
-					// The 'this' is the settings object, can be modified
-				},
-				onAfter:function( anchor, settings ){
-					// The 'this' contains the scrolled element (#content)
-				}
-			});
+			It's not really working on the search page 
+			*/
+		$("a[href^='#']").mPageScroll2id({
+    		scrollSpeed: 1300,
+    		scrollEasing: "easeInOutQuad",
+    		pageEndSmoothScroll: true
+		});
 	});
 });
