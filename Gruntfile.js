d97f48f256b49c97b9f17bda9af4c5fd0b0f4c01
Paul Craig <pcraig3@uwo.ca>
2016-04-07 00:56:59 +0100

    Add sweet-scroll and postcss stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,9 @@ module.exports = function(grunt) {
         concat: {   
 		    dist: {
 		        src: [
-                    'js/bower_components/page-scroll-to-id/*.js',  // This specific file
-                    'js/bower_components/chardin.js/chardinjs.js',  // This specific file
+                    'js/bower_components/page-scroll-to-id/*.js',
+                    'js/bower_components/chardin.js/chardinjs.js',
+					'node_modules/sweet-scroll/sweet-scroll.js',
 		            'js/back_to_top.js',
 		            'js/scroll.js',
                     'js/chardin.js',
@@ -24,13 +25,29 @@ module.exports = function(grunt) {
 			}
 		},
 
-		uglify: {
-		    build: {
-		    	  files: {
-        			'js/build/prod.min.js': ['js/build/prod.js'],
-        			'js/build/search.min.js': ['js/build/search.js']
-      			}
-		    }
+		postcss: {
+			// use browser prefixes when required
+			options: {
+				map: true,
+				processors: [
+					require('autoprefixer')(
+						// https://github.com/twbs/bootstrap-sass#sass-autoprefixer
+						{browsers: [
+							"Android 2.3",
+							"Android >= 4",
+							"Chrome >= 20",
+							"Firefox >= 24",
+							"Explorer >= 8",
+							"iOS >= 6",
+							"Opera >= 12",
+							"Safari >= 6"
+						]}
+					)
+				]
+			},
+			dist: {
+				src: 'style.css'
+			}
 		},
 
 		sass: {
@@ -42,6 +59,15 @@ module.exports = function(grunt) {
 					'style.min.css': 'style.scss' // 'destination': 'source'
 				}
 			}
+		},
+
+		uglify: {
+		    build: {
+		    	  files: {
+        			'js/build/prod.min.js': ['js/build/prod.js'],
+        			'js/build/search.min.js': ['js/build/search.js']
+      			}
+		    }
 		},
 
 		watch: {
@@ -61,6 +87,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-postcss');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['concat', 'uglify', 'sass']);
