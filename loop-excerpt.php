e99bb80afbcedf443fb4d1c48604149559e7016e
Paul Craig <pcraig3@uwo.ca>
2016-09-06 17:05:36 +0100

    Update archive page markup.
    Wrapping each archive listing in an anchor tag was doing weird
    things with the focus outline for no real benefit.
    Moved the wrapping anchor tag into the article and nothing seemed
    to change.
    
    Also removed an unecessary line of CSS.

--- loop-excerpt.php
+++ loop-excerpt.php
@@ -1,14 +1,13 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	 <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e('Link to','zweig'); ?> <?php the_title_attribute(); ?>" itemprop="url"><article itemtype="http://schema.org/BlogPosting"  id="post-<?php the_ID(); ?>" <?php post_class('category'); ?>>
+	<article itemtype="http://schema.org/BlogPosting"  id="post-<?php the_ID(); ?>" <?php post_class('category'); ?>>
+		<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e('Link to','zweig'); ?> <?php the_title_attribute(); ?>" itemprop="url">
 
-		<h1 itemprop="headline" class="post-title"><?php the_title(); ?></h1>
+			<h1 itemprop="headline" class="post-title"><?php the_title(); ?></h1>
+			<h2 class="post-date"><?php the_time('F Y'); ?></h2>
 
-		 <h2 class="post-date">
-		 	<?php the_time('F Y'); ?>
-		 </h2>
-
-	 </article></a>
+		</a>
+	 </article>
 
 	 <!-- Stop The Loop (but note the "else:" - see next line). -->
 	 <?php endwhile; else: ?>
