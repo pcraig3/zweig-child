b648a4ac574fc8f3fa0416fb2cdd2648ae713d85
Paul Craig <pcraig3@uwo.ca>
2016-09-06 16:07:36 +0100

    Create new grunt task for JS and one for CSS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,5 +100,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-postcss');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'postcss']);
+	grunt.registerTask('default', ['concat', 'uglify', 'sass', 'postcss']);
+	grunt.registerTask('css', ['sass', 'postcss']);
+	grunt.registerTask('js', ['concat', 'uglify']);
 };
