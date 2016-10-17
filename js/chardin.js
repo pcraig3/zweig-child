80e2fd79442e3ff7b6367c87264eca80b9b63092
Paul Craig <pcraig3@uwo.ca>
2016-10-18 00:28:38 +0100

    trap focus in overlay when it's open.
    also added a role="dialog" to the top-level chardinjs thing.

--- js/chardin.js
+++ js/chardin.js
@@ -4,7 +4,7 @@ jQuery(function( $ ){
     $body = $('body');
     focus_on_close_class = 'chardinjs--focus-on-close';
 
-    if( utils.is_mobile() ) {
+    if ( utils.is_mobile() ) {
         $body.addClass('is-mobile');
     }
 
@@ -23,23 +23,17 @@ jQuery(function( $ ){
         e.preventDefault();
     });
 
-    function chardinjs_stop(e){
+    function chardinjs_stop(e) {
         $body.chardinJs('stop');
         $body.find('.' + focus_on_close_class).removeClass(focus_on_close_class).focus();
         $(document).off('keydown');
-        if(e) {
+        if (e) {
             e.preventDefault();
         }
     }
 
-    $(document).keydown(function(e) {
-        console.log('herE!');
-        console.log(e.keyCode);
-
-    });
-
     function chardinjs_stop_keydown_event_handler(e){
-        if(
+        if (
             e.keyCode == 8      // backspace
             || e.keyCode == 27  // esc
             || e.keyCode == 88  // x
@@ -52,8 +46,23 @@ jQuery(function( $ ){
 
         $(this).find('.chardinjs--stop').one('click', chardinjs_stop);
         $(document).one('keydown', chardinjs_stop_keydown_event_handler);
-        $(this).find('.chardinjs-overlay').attr('tabindex', -1).focus();
+        $(this).find('.chardinjs-overlay').attr('role', 'dialog').attr('tabindex', -1).focus();
         // override the default opacity
         setTimeout(function() { $('.chardinjs-overlay').css('opacity', .95); }, 30);
+    });
+
+    /*
+    listen to focus events.
+    if chardin-js is activated but the focused
+    element is not inside of the overlay, then
+    we reset the focus to the top of the overlay
+     */
+    $(document).on("focus", "a, button", function(e) {
+        var $overlay = $body.find('.chardinjs-overlay');
+
+        if ($overlay.length && !($(this).parents('.chardinjs-helper-layer, #site-logo').length)) {
+            e.stopPropagation();
+            $overlay.focus();
+        }
     });
 });
