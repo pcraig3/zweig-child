0d813bbdd1db70e35178863c1d5583afc1219d8b
pcraig3 <paul_craig_16@hotmail.com>
2017-05-01 23:14:11 +0100

    pretty passable post processing
    Slight improvement in output over the grunt tasks.
    Huge improvement in speed though.
    
    _whoosh_

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var csscomb = require('gulp-csscomb');
+var csso = require('gulp-csso');
+var gulpif = require('gulp-if');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
@@ -7,6 +11,20 @@ function compileSass(gulp, outputStyle, filename) {
     return gulp.src('style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: outputStyle}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: [
+                "Android 2.3",
+                "Android >= 4",
+                "Chrome >= 20",
+                "Firefox >= 24",
+                "Explorer >= 8",
+                "iOS >= 6",
+                "Opera >= 12",
+                "Safari >= 6"
+            ]
+        }))
+        .pipe(csso())
+        .pipe(gulpif((outputStyle === 'expanded'), csscomb()))
         .pipe(rename(filename))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('.'))
